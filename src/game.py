b64c023b3cf52103d9865d8b7508d95f2d58b520
Frederik <fjaplv64@gmail.com>
2019-04-27 09:12:43 +0200

    Initial commit v2

--- src/game.py
+++ src/game.py
@@ -1,2 +1,35 @@
 import pygame
 
+class Game:
+    def __init__(self):
+        pygame.init()
+
+        self.playtime = 0
+        self.running = True
+
+        self.screen = pygame.display.set_mode([1028, 720])
+        self.background = pygame.Surface(self.screen.get_size())
+        self.background.fill((255, 255, 255))
+        self.background = self.background.convert()
+
+    def main(self):
+        while self.running:
+            self.on_update()
+            self.on_draw()
+
+    def on_draw(self):
+        self.screen.blit(self.background, (0, 0))
+
+        pygame.display.flip()
+
+    def on_update(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    self.running = False
+
+        clock = pygame.time.Clock()
+        milliseconds = clock.tick(30)
+        self.playtime += milliseconds / 1000.0
--- src/main.py
+++ src/main.py
@@ -1,30 +1,4 @@
-import pygame
+from src.game import Game
 
-pygame.init()
-
-playtime = 0
-running = True
-
-screen = pygame.display.set_mode([1028, 720])
-background = pygame.Surface(screen.get_size())
-background.fill((255, 255, 255))
-background = background.convert()
-
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                running = False
-
-    clock = pygame.time.Clock()
-    milliseconds = clock.tick(30)
-    playtime += milliseconds / 1000.0
-
-    screen.blit(background, (0, 0))
-
-    pygame.display.flip()
-
-
-
+game = Game()
+game.main()
